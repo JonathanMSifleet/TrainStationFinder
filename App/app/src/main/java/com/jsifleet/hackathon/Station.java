e14aa8ced58ea34372777bd8b091ffb38819ea9c
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-04-01 19:52:14 +0100

    Revert "Revert "Add comments and javadoc""
    This reverts commit 4367ea0211fc37ddcf1f1074b6c0dff13b879383.

--- App/app/src/main/java/com/jsifleet/hackathon/Station.java
+++ App/app/src/main/java/com/jsifleet/hackathon/Station.java
@@ -5,38 +5,71 @@ public class Station {
 	private String StationName;
 	private double lat;
 	private double lng;
+	private double distance;
 
+	// getters and setters for Station variables:
+
+	/**
+	 * Retrieves station's distance
+	 * @return Station's distance (double)
+	 */
 	public double getDistance() {
 		return distance;
 	}
 
+	/**
+	 * Sets station's distance
+	 * @param Station's distance (double)
+	 */
 	public void setDistance(double distance) {
 		this.distance = distance;
 	}
 
-	private double distance;
-
+	/**
+	 * Retrieves station's name
+	 * @return Station's name (String)
+	 */
 	public String getStationName() {
 		return StationName;
 	}
 
+	/**
+	 * Sets station's name
+	 * @param Station's name (String)
+	 */
 	public void setStationName(String stationName) {
 		StationName = stationName;
 	}
 
-	public Double getLat() {
+	/**
+	 * Retrieves station's latitude
+	 * @return Station's latitude (double)
+	 */
+	public double getLat() {
 		return lat;
 	}
 
-	public void setLat(Double lat) {
+	/**
+	 * Sets station's latitude
+	 * @param Stations latitude (double)
+	 */
+	public void setLat(double lat) {
 		this.lat = lat;
 	}
 
-	public Double getLng() {
+	/**
+	 * Retrieves station's longitude
+	 * @return Station's longitude (double)
+	 */
+	public double getLng() {
 		return lng;
 	}
 
-	public void setLng(Double lng) {
+	/**
+	 * Sets station's longitude
+	 * @param Station's longitude double)
+	 */
+	public void setLng(double lng) {
 		this.lng = lng;
 	}
 
--- App/app/src/main/java/com/jsifleet/hackathon/webService.java
+++ App/app/src/main/java/com/jsifleet/hackathon/webService.java
@@ -12,23 +12,34 @@ import java.net.URL;
 
 public class WebService {
 
+	/**
+	 * Gets a JSON Array of all the nearby train stations, based upon the latitude and longitude supplied
+	 * @param Device's latitude (double)
+	 * @param Device's longitude (double)
+	 * @return JSON Array containing all station data
+	 */
 	public JSONArray getStationsFromURL(double lat, double lng) {
-
+		// pass lat and long to URL function
 		URL u = buildURL(lat, lng);
-		//Log.e("URL", u.toString());
 		try {
+			// open HTTP connection to URL
 			HttpURLConnection tc = (HttpURLConnection) u.openConnection();
+			// create readers for data from HTTP connection
 			InputStreamReader isr = new InputStreamReader(tc.getInputStream());
 			BufferedReader in = new BufferedReader(isr);
 
 			String line;
 			StringBuilder json = new StringBuilder();
 
+			// append all lines from URL to string
 			while ((line = in.readLine()) != null) {
 				json.append(line);
 			}
+			// close readers
 			in.close();
+			isr.close();
 
+			// return encoded string as JSONArray
 			return new JSONArray(json.toString());
 
 		} catch (IOException | JSONException e) {
@@ -37,7 +48,14 @@ public class WebService {
 		return new JSONArray();
 	}
 
+	/**
+	 * Builds a URL to use for getting nearby train stations, based upon the latitude and longitude supplied
+	 * @param Device's latitude (double)
+	 * @param Device's longitude (double)
+	 * @return URL to use for getting nearby station
+	 */
 	public static URL buildURL(double lat, double lng) {
+		// build URL based on latitude and longitude:
 		try {
 			return new URL("http://10.0.2.2:8080/stations?lat=" + lat + "&lng=" + lng);
 		} catch (MalformedURLException e) {
