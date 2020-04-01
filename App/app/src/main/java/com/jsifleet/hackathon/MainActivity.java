35049422ff7289075fbbd7a06a2d16f3534c2762
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-04-01 09:23:27 +0100

    Get device location

--- App/app/src/main/java/com/jsifleet/hackathon/MainActivity.java
+++ App/app/src/main/java/com/jsifleet/hackathon/MainActivity.java
@@ -1,13 +1,89 @@
 package com.jsifleet.hackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	Double deviceLat = 0.0;
+	Double deviceLng = 0.0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+
+		this.getLocation();
+
+	}
+
+	public void getLocation() {
+
+		String[] locationPermissions = {
+				Manifest.permission.INTERNET,
+				Manifest.permission.ACCESS_FINE_LOCATION,
+				Manifest.permission.ACCESS_COARSE_LOCATION
+		};
+
+		if (checkGotPermission(locationPermissions)) {
+
+			LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+			lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+				@Override
+				public void onLocationChanged(Location location) {
+					deviceLat = location.getLatitude();
+					deviceLng = location.getLongitude();
+					//Log.e("Location:", deviceLat.toString() + ", " + deviceLng.toString());
+				}
+
+				@Override
+				public void onStatusChanged(String provider, int status, Bundle extras) {
+
+				}
+
+				@Override
+				public void onProviderEnabled(String provider) {
+
+				}
+
+				@Override
+				public void onProviderDisabled(String provider) {
+
+				}
+			});
+		}
+
+	}
+
+	public boolean checkGotPermission(String[] requiredPermissions) {
+
+		boolean ok = true;
+		for (int i = 0; i < requiredPermissions.length; i++) {
+			int result = ActivityCompat.checkSelfPermission(this, requiredPermissions[i]);
+			if (result != PackageManager.PERMISSION_GRANTED) {
+				ok = false;
+			}
+		}
+
+		if (!ok) {
+			ActivityCompat.requestPermissions(this, requiredPermissions, 1);
+			// last permission must be > 0
+		} else {
+			return true;
+		}
+		return false;
+	}
 }
