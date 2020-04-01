4367ea0211fc37ddcf1f1074b6c0dff13b879383
JonathanMSifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-04-01 19:46:19 +0100

    Revert "Add comments and javadoc"
    This reverts commit 0ff7875016d17e3a5870776e386198693fec0882.

--- App/app/src/main/java/com/jsifleet/hackathon/Station.java
+++ App/app/src/main/java/com/jsifleet/hackathon/Station.java
@@ -5,71 +5,38 @@ public class Station {
 	private String StationName;
 	private double lat;
 	private double lng;
-	private double distance;
 
-	// getters and setters for Station variables:
-
-	/**
-	 * Retrieves station's distance
-	 * @return Station's distance (double)
-	 */
 	public double getDistance() {
 		return distance;
 	}
 
-	/**
-	 * Sets station's distance
-	 * @param Station's distance (double)
-	 */
 	public void setDistance(double distance) {
 		this.distance = distance;
 	}
 
-	/**
-	 * Retrieves station's name
-	 * @return Station's name (String)
-	 */
+	private double distance;
+
 	public String getStationName() {
 		return StationName;
 	}
 
-	/**
-	 * Sets station's name
-	 * @param Station's name (String)
-	 */
 	public void setStationName(String stationName) {
 		StationName = stationName;
 	}
 
-	/**
-	 * Retrieves station's latitude
-	 * @return Station's latitude (double)
-	 */
-	public double getLat() {
+	public Double getLat() {
 		return lat;
 	}
 
-	/**
-	 * Sets station's latitude
-	 * @param Stations latitude (double)
-	 */
-	public void setLat(double lat) {
+	public void setLat(Double lat) {
 		this.lat = lat;
 	}
 
-	/**
-	 * Retrieves station's longitude
-	 * @return Station's longitude (double)
-	 */
-	public double getLng() {
+	public Double getLng() {
 		return lng;
 	}
 
-	/**
-	 * Sets station's longitude
-	 * @param Station's longitude double)
-	 */
-	public void setLng(double lng) {
+	public void setLng(Double lng) {
 		this.lng = lng;
 	}
 
